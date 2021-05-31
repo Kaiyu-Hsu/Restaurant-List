0af812b680bc4d8d341e85962420c2bb8b64d98c
Kaiyu <anne550323@gmail.com>
2021-05-31 15:27:12 +0800

    js設定路由 handlebars設定基本頁面

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3001
+
+//告訴 Express 靜態檔案是放在名為 public 的資料夾中
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen( port, () => {
+  console.log(`Express is listening on http://localhost:${port}`)
+})
