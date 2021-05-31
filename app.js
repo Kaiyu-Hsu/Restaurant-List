95f92e7fc030df730f86208b4780f662660a508c
Kaiyu <anne550323@gmail.com>
2021-05-31 16:33:09 +0800

    app.js setting template engine & revise index css

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// require express-handlebars here
+const exphbs = require('express-handlebars')
+
 //告訴 Express 靜態檔案是放在名為 public 的資料夾中
 app.use(express.static('public'))
 
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting router
 app.get('/', (req, res) => {
   res.render('index')
 })
