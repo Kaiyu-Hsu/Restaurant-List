3b059cb0b5c6756c1ca088c29914cdfffa53e4f4
Kaiyu <anne550323@gmail.com>
2021-06-01 16:07:34 +0800

    js增加餐廳資訊路由 修改show page

--- app.js
+++ app.js
@@ -13,9 +13,15 @@ app.use(express.static('public'))
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-// setting router
+// setting router 首頁
 app.get('/', (req, res) => {
   res.render('index', { restaurants: restaurantList.results })
+})
+
+// 更多內容 => 餐廳資訊
+app.get('/restaurants/:restaurant_id', (req, res) => {
+  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id )
+  res.render('show', { restaurant: restaurant })
 })
 
 app.listen( port, () => {
