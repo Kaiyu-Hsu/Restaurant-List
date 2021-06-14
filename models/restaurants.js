23a66a0f210ba294e9c6bf452a87a87f2360e12e
Kaiyu <anne550323@gmail.com>
2021-06-14 18:12:41 +0800

    feat: modify restaurantSchema

--- models/restaurants.js
+++ models/restaurants.js
@@ -4,6 +4,35 @@ const restaurantSchema = new Schema({
   name: {
     type: String, // 資料型別是字串
     require: true, // 這是個必填欄位
+  },
+  name_en: {
+    type: String
+  },
+  category: {
+    type: String,
+    require: true
+  },
+  image: {
+    type: String
+  },
+  location: {
+    type: String,
+    require: true
+  },
+  phone: {
+    type: String,
+    require: true
+  },
+  google_map: {
+    type: String
+  },
+  rating: {
+    type: Number,
+    require: true
+  },
+  description: {
+    type: String
   }
 })
+
 module.exports = mongoose.model('Restaurants', restaurantSchema)
